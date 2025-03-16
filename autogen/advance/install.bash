7a197f6dbb153969f56fddabd29269581d3177fd
kevin <guangjinhai@163.com>
2025-03-16 22:09:28 +0800

    update folder

---
+++ autogen/advance/install.bash
@@ -0,0 +1,4 @@
+pip install "autogen-agentchat" "autogen-ext[magentic-one,openai]"
+
+# If using the MultimodalWebSurfer, you also need to install playwright dependencies:
+playwright install --with-deps chromium
